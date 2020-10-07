f0fe5ee5d2e33b86fc4908fabfc228090322f078
roman <romka69tver@gmail.com>
2020-10-07 20:22:02 +0300

    add time createa_at for tasks

--- src/components/Tasks/Task.vue
+++ src/components/Tasks/Task.vue
@@ -7,16 +7,31 @@
     <q-item-section side top>
       <q-checkbox :value="task.completed" class="no-pointer-events" />
     </q-item-section>
-    
+
     <q-item-section>
       <q-item-label>
         {{ task.title }}
       </q-item-label>
     </q-item-section>
+
+    <q-item-section side>
+      <div class="row">
+        <div class="column justify-center">
+          <q-icon name="eva-calendar-outline" size="xs" class="q-mr-xs" />
+        </div>
+        <div class="column">
+          <q-item-label class="row justify-end" caption>
+            {{ task.created_at | goodDate }}
+          </q-item-label>
+        </div>
+      </div>
+    </q-item-section>
   </q-item>
 </template>
 
 <script>
+import { date } from "quasar";
+
 export default {
   name: "Task",
 
@@ -32,7 +47,17 @@ export default {
     completed: {
       item: Boolean,
       default: false
-    }
+    },
+    created_at: {
+      item: String,
+      default: ""
+    },
+  },
+
+  filters: {
+    goodDate(value) {
+      return date.formatDate(Date.parse(value), "D MMM YY");
+    },
   },
 };
 </script>
